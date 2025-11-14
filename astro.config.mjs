27163a3fe3ead5b7653341a533388780858ac958
Lukáš Hejtmánek <hejtmy@gmail.com>
2025-11-14 20:38:01 +0100

    updates the web

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,11 +10,8 @@ export default defineConfig({
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
+					label: 'Úvod do psychologie',
+          autogenerate: { directory: 'introduction-to-psychology' },
 				},
 				{
 					label: 'Reference',
